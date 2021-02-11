b4fdef86f336649632c944ff141fdd1962f22915
MSBarbieri <matheussouzabarbieri@gmail.com>
2021-02-11 20:48:59 -0300

    start

---
+++ graphics-programming.cpp
@@ -0,0 +1,12 @@
+﻿// graphics-programming.cpp : Defines the entry point for the application.
+//
+
+#include "graphics-programming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
